1e6b4244b126282a822269ca23e4bda5f7cf1522
carlOzil <seifer01es@msn.com>
2023-08-23 23:00:57 +0200

    all needed dependencies in

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require ('express');
+const cors = require ('cors');
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 
@@ -7,6 +9,18 @@ const app = express();
 
 //PORT
 const port = process.env.PORT;
+
+//CORS
+app.use(cors());
+
+//COOKIEPARSER
+app.use(cookieParser());
+
+//parse application/x-www-form-urlencoded 
+app.use(express.urlencoded({ extended: false }))
+
+//parse application/json
+app.use(express.json())
 
 //SERVIDOR A LA ESCUCHA
 app.listen(port, () => {
